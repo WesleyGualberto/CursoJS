42e3269d888686a0b4d6adeb234049331474e9dd
Wesley Gualberto <wesleygualberto0@gmail.com>
2025-01-16 08:53:01 -0300

    Feito exercicios JS
    Feito exercícios  de condições if and else

---
+++ CURSOJS/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+
+if(vel > 60) { // Codicao simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando o cinto de seguranca!')
---
+++ CURSOJS/aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+// Condicao composta (if == "se") e (else == "senao")
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!');
+} else {
+    console.log('Você é Estrangeiro!');
+}
