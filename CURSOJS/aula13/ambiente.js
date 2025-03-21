475dba8de00fb98a90a33491ad85a492f15ebd28
Wesley Gualberto <wesleygualberto0@gmail.com>
2025-03-21 07:26:05 -0300

    feito exercicios de while e dowhile

---
+++ CURSOJS/aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*
+While - Estrutura de repeticao com teste logico no inicio.
+*/
+
+var contador = 1
+
+while (contador <= 6) {
+    console.log('Tudo bem?')
+    contador++
+}
+
+/* 
+do while - Estrutura de repeticao com teste logico no final.
+*/
+
+var contador = 1
+
+do {
+    console.log('Tudo bem?')
+    contador++
+} while (contador <= 6)
